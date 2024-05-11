c5b866a51d3be4adf466eb669375677033b824b7
Kayky Thiago <147095292+kaykyth181@users.noreply.github.com>
2024-05-11 11:30:50 -0300

    update js theme

--- js/script.js
+++ js/script.js
@@ -3,17 +3,27 @@
 
 
 //vai até o index e pega o toggleTheme
-const toggleTheme = document.getElementsById("toggleTheme");
+const toggleTheme = document.getElementById("toggleTheme");
 
 const rootHtml = document.documentElement;
 
 // querySelectorAll pega todos os elementos com o nome
 const accordionHeader = document.querySelectorAll(".accordion-header");
 
-const menuLinks = document.querySelectorAll(".menu-link")
+const menuLinks = document.querySelectorAll(".menu-link");
 
 function changeTheme(){
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
     
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
 }
+
+toggleTheme.addEventListener("click", changeTheme);
+
+//fim do toggle-theme
+
 
 
