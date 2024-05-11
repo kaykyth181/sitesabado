f941a3ef4a270cd74a1636174c3c3628f5104f73
Kayky Thiago <147095292+kaykyth181@users.noreply.github.com>
2024-05-11 10:09:45 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//linha
+/*comentario*/
+
+
+//vai até o index e pega o toggleTheme
+const toggleTheme = document.getElementsById("toggleTheme");
+
+const rootHtml = document.documentElement;
+
+// querySelectorAll pega todos os elementos com o nome
+const accordionHeader = document.querySelectorAll(".accordion-header");
+
+const menuLinks = document.querySelectorAll(".menu-link")
+
+function changeTheme(){
+    
+}
+
+
